7d3613861365ed2244c75a691d2f25db5e99ce5f
ottovbismarck <fritzj96@gmail.com>
2019-06-25 02:22:17 -0700

    created basic passport strategy

--- src/config/passport-strat-options.ts
+++ src/config/passport-strat-options.ts
@@ -1,10 +1,25 @@
 // Package Imports
-import { ExtractJwt, StrategyOptions } from "passport-jwt";
+import { ExtractJwt, StrategyOptions, Strategy } from "passport-jwt";
 
 // StrategyOptions object to add options to
-export const opts: StrategyOptions = {
+export const passportSetup = (passport) => {
+  const opts: StrategyOptions = {
   // defines where/how to extract the JWT
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   // defines whether the strat should use a key or a secret
   secretOrKey: "secret",
+  };
+  passport.use(new Strategy(opts, (jwt_payload, done) => {
+    User.getUserById(jwt_payload.data._id, (err, user) => {
+      if (err) {
+        return done(err, false);
+      }
+
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    });
+  }));
 };
