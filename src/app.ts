2b905805d12ad05bfbfca65532aeaaef904cb662
ottovbismarck <fritzj96@gmail.com>
2019-06-25 02:09:07 -0700

    commenting

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,10 @@ import express from "express";
 
 // Environment Vars
 const port = process.env.PORT;
-
+// App Initialization
 const app = express();
 
+// Starts The Server Listening For Requests
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
