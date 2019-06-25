d09244eb7dead79605af8e8833630e5b85bfbf01
ottovbismarck <fritzj96@gmail.com>
2019-06-25 02:53:38 -0700

    installed CORS middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 // Package Imports
 import express from "express";
+import passport from "passport";
 
 // Environment Vars
 const port = process.env.PORT;
 // App Initialization
 const app = express();
+
+// Global Middleware Setup
+app.use()
 
 // Starts The Server Listening For Requests
 app.listen(port, () => {
